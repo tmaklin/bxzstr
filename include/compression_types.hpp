aa81c93e0b1096e24817b9eb784117c0cae4f0ad
Tommi Mäklin <tommi@maklin.fi>
2019-11-21 16:48:20 +0200

    inline functions to avoid compiler issues & ifdef supported types

--- include/compression_types.hpp
+++ include/compression_types.hpp
@@ -17,49 +17,73 @@
 
 namespace bxz {
     enum Compression { z, bz2, lzma, plaintext };
-    Compression detect_type(char* in_buff_start, char* in_buff_end) {
+    inline Compression detect_type(char* in_buff_start, char* in_buff_end) {
 	unsigned char b0 = *reinterpret_cast< unsigned char * >(in_buff_start);
 	unsigned char b1 = *reinterpret_cast< unsigned char * >(in_buff_start + 1);
+#ifdef BXZSTR_Z_STREAM_WRAPPER_HPP
         if (in_buff_start + 2 <= in_buff_end
 	    && ((b0 == 0x1F && b1 == 0x8B)         // gzip header
 		|| (b0 == 0x78 && (b1 == 0x01      // zlib header
 				   || b1 == 0x9C
 				   || b1 == 0xDA))))
 	    return z;
+#endif
 	unsigned char b2 = *reinterpret_cast< unsigned char * >(in_buff_start + 2);
+#ifdef BXZSTR_BZ_STREAM_WRAPPER_HPP
         if (in_buff_start + 2 <= in_buff_end
 	    && ((b0 == 0x42 && b1 == 0x5a && b2 == 0x68))) // bz2 header
 	    return bz2;
+#endif
 	unsigned char b3 = *reinterpret_cast< unsigned char * >(in_buff_start + 3);
 	unsigned char b4 = *reinterpret_cast< unsigned char * >(in_buff_start + 4);
 	unsigned char b5 = *reinterpret_cast< unsigned char * >(in_buff_start + 5);
+#ifdef BXZSTR_LZMA_STREAM_WRAPPER_HPP
 	if (in_buff_start + 6 <= in_buff_end
 	    && ((b0 == 0xFD && b1 == 0x37 && b2 == 0x7A
 		 && b3 == 0x58 && b4 == 0x5A && b5 == 0x00))) // liblzma header
 	    return lzma;
+#endif
 	return plaintext;
     }
-    void init_stream(const Compression &type, detail::stream_wrapper **strm_p, bool is_input = true) {
+    inline void init_stream(const Compression &type, detail::stream_wrapper **strm_p, bool is_input = true) {
 	switch (type) {
+#ifdef BXZSTR_LZMA_STREAM_WRAPPER_HPP
 	case lzma : *strm_p = new detail::lzma_stream_wrapper(is_input); break;
+#endif
+#ifdef BXZSTR_BZ_STREAM_WRAPPER_HPP
 	case bz2 : *strm_p = new detail::bz_stream_wrapper(is_input); break;
+#endif
+#ifdef BXZSTR_Z_STREAM_WRAPPER_HPP
 	case z : *strm_p = new detail::z_stream_wrapper(is_input); break;
+#endif
 	default : throw std::runtime_error("Unrecognized compression type.");
 	}
     }
-    int bxz_run(const Compression &type){
+    inline int bxz_run(const Compression &type){
 	switch(type){
-        case lzma: return 0; break; // LZMA_RUN
+#ifdef BXZSTR_LZMA_STREAM_WRAPPER_HPP
+	case lzma: return 0; break; // LZMA_RUN
+#endif
+#ifdef BXZSTR_BZ_STREAM_WRAPPER_HPP
         case bz2: return 0; break; // BZ_RUN
+#endif
+#ifdef BXZSTR_Z_STREAM_WRAPPER_HPP
         case z: return 0; break; // Z_NO_FLUSH
+#endif
 	default: throw std::runtime_error("Unrecognized compression type.");
 	}
     }
-    int bxz_finish(const Compression &type){
+    inline int bxz_finish(const Compression &type){
 	switch(type){
-        case lzma: return 3; break; // LZMA_FINISH
+#ifdef BXZSTR_LZMA_STREAM_WRAPPER_HPP
+	case lzma: return 3; break; // LZMA_FINISH
+#endif
+#ifdef BXZSTR_BZ_STREAM_WRAPPER_HPP
         case bz2: return 2; break; // BZ_FINISH
+#endif
+#ifdef BXZSTR_Z_STREAM_WRAPPER_HPP
         case z: return 4; break; // Z_FINISH
+#endif
 	default: throw std::runtime_error("Unrecognized compression type.");
 	}
     }
